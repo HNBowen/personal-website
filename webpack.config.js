d20482d0f1e458774775f17efd6dbca075f22fc5
Hank <hbowen1123@gmail.com>
2017-09-14 20:28:35 -0500

    Webpack/Babel config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js','.json']
+  }
+};
