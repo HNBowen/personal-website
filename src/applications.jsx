b249fb963426873611b9b296cb5783ce1cc45176
Hank <hbowen1123@gmail.com>
2017-09-16 14:48:24 -0500

    added applications section

---
+++ src/applications.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Applications = () => {
+  return (
+      <div className="applications-container">
+        <h4>Applications</h4>
+        <article>brainstorm</article>
+        <article>ShareMyKitchen</article>
+        <article>GoodFilms</article>
+      </div>
+    )
+}
+
+export default Applications
