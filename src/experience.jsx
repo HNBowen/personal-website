f3f52bad9b17d1388b8fc810599f148f801ed12c
Hank <hbowen1123@gmail.com>
2017-09-16 14:48:09 -0500

    Added experience section

---
+++ src/experience.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Experience = () => {
+  return (
+      <div className="experience-container">
+        <h4>Experience</h4>
+        <article>Hack Reactor
+          <ul>
+            <li>JavaScript Immersive</li>
+            <li>3-month post-graudation residency</li>
+          </ul>
+        </article>
+        <article>University of New Hampshire
+          <ul>
+            <li>B.S. Physics</li>
+            <li>International Affiars Dual Major</li>
+            <li>French minor</li>
+          </ul>
+        </article>
+      </div>
+    )
+}
+
+export default Experience
