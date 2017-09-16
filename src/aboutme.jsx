fe08ca7226f8ac291cfd4c88b79cc56abde6dded
Hank <hbowen1123@gmail.com>
2017-09-16 14:47:21 -0500

    Added aboutme section

---
+++ src/aboutme.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutMe = () => {
+  return (
+      <div className = 'about-me'>
+        <p>
+          I am a full-stack software engineer who is passionate about creating functional, useful and beautiful applications. 
+        </p>
+      </div>
+    )
+}
+
+export default AboutMe
